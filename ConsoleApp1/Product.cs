a0b935074db47b9709eb2451afd1d3fefabcf569
4115675 <4115675@TE-GDA-D-D21E14>
2025-03-24 10:37:52 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // Klasa Product reprezentuje produkt w systemie
+    class Product
+    {
+        // Właściwości klasy Product:
+        public string BarCode;    // Kod kreskowy produktu (unikalny identyfikator)
+        public string Name;        // Nazwa produktu
+        public double Price;       // Cena produktu
+
+        // Konstruktor klasy Product - służy do tworzenia nowych obiektów Product
+        // Parametry:
+        //   barCode - kod kreskowy produktu
+        //   name - nazwa produktu
+        //   price - cena produktu
+        public Product(string barCode, string name, double price)
+        {
+            BarCode = barCode;  // Przypisanie wartości kodu kreskowego
+            Name = name;        // Przypisanie nazwy produktu
+            Price = price;      // Przypisanie ceny produktu
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿using ConsoleApp1;
+
+Product p = new Product("1234567890", "Mleko", 2.5);
+Console.WriteLine("produkt o nazwie : " + p.Name + " | kosztuje - " + p.Price + " zł, a jego kod kreskowy to : " + p.BarCode);
